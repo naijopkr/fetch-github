cca8f2c9f8dbf09deda0448d769c69def4816b71
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-03-24 14:58:47 -0500

    adds parameter to sort and increase response results

--- github.js
+++ github.js
@@ -1,16 +1,13 @@
 const fs = require('fs')
 const fetch = require('node-fetch')
 
-fetch('https://api.github.com/users/naijopkr/repos?type=owner')
+fetch('https://api.github.com/users/naijopkr/repos?type=owner&sort=updated&per_page=100')
     .then(res => {
         res.json().then(body => {
             const repos = {
                 data: body
                 .filter(repo => !repo.fork)
                 .map(({ id, name, html_url, description, language, updated_at }) => {
-                    if (Array.isArray(language)) {
-                        console.log(language)
-                    }
 
                     return {
                         id,
@@ -21,7 +18,6 @@ fetch('https://api.github.com/users/naijopkr/repos?type=owner')
                         updated_at,
                     }
                 })
-                .sort((a, b) => new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime())
             }
 
             fs.writeFile('github.json', JSON.stringify(repos, null, 2), err => {
